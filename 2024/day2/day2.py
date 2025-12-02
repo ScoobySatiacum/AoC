9b50a15b715542071f791420db9291f8409169ef
Skyler Satiacum <skylersatiacum@gmail.com>
2025-12-02 06:12:16 -0800

    Day 2 part 1 complete

---
+++ 2024/day2/day2.py
@@ -0,0 +1,48 @@
+def validate_report(report):
+    report_levels = report.split(' ')
+    report_levels = [int(i) for i in report_levels]
+
+    direction = 'increasing' if report_levels[0] < report_levels[1] else 'decreasing'
+    
+    for i in range(len(report_levels) - 1):
+        if report_levels[i] > report_levels[i + 1]:
+            if direction == 'increasing':
+                return False
+            else:
+                if report_levels[i] - report_levels[i + 1] > 0:
+                    if report_levels[i] - report_levels[i + 1] < 4:
+                        pass
+                    else:
+                        return False
+                else:
+                    return False
+        else:
+            if direction == 'decreasing':
+                return False
+            else:
+                if report_levels[i + 1] - report_levels[i] > 0:
+                    if report_levels[i + 1] - report_levels[i] < 4:
+                        pass
+                    else:
+                        return False
+                else:
+                    return False
+    
+    return True
+
+if __name__ == '__main__':
+
+    inputs_path = r"C:\Users\skyle\Documents\dev\AoC\2024\day2\input.txt"
+
+    inputs = []
+    with open(inputs_path, 'r', encoding='utf-8') as fo:
+        inputs = [r.replace("\n", "") for r in fo.readlines()]
+
+    safe_reports = []
+    unsafe_reports = []
+
+    for report in inputs:
+        if validate_report(report):
+            safe_reports.append(report)
+
+    print(len(safe_reports))
