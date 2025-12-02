a62e6012936b346809323e1e34cb0b82cbf4db46
Skyler Satiacum <skylersatiacum@gmail.com>
2025-12-02 09:49:29 -0800

    Completed 2025 day 2 part 2

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -1,4 +1,4 @@
-def validate_ids(id_range):
+def validate_ids_part1(id_range):
 
     valid_ids = []
     
@@ -11,6 +11,32 @@ def validate_ids(id_range):
 
             if pattern in content:
                 valid_ids.append(int(item))
+
+    return valid_ids
+
+def validate_ids_part2(id_range):
+    
+    minimum = 1
+    maximum = len(id_range[-1]) // 2
+    pattern_length_options = [i for i in range(minimum, maximum + 1)]
+
+    valid_ids = []
+
+    for len_option in pattern_length_options:    
+        for item in id_range:
+
+            pattern_length = len_option
+
+            pattern = item[0:pattern_length]
+            content = item[pattern_length:len(item)]
+
+            if content:
+
+                split_content = item.split(pattern)
+                test = [i for i in split_content if i != '']
+                if not test:
+                    if int(item) not in valid_ids:
+                        valid_ids.append(int(item))
 
     return valid_ids
 
@@ -30,8 +56,23 @@ if __name__ == '__main__':
         beginning, end = int(products_split[0]), int(products_split[1])
         product_range = [str(i) for i in range(beginning, end + 1)]
 
-        valid_ids = validate_ids(product_range)
+        valid_ids = validate_ids_part1(product_range)
         if valid_ids:
             answers.extend(valid_ids)
+
+    print(sum(answers))
+
+    answers = []
+
+    # parse ranges
+    for products in ranges:
+        products_split = products.split('-')
+        beginning, end = int(products_split[0]), int(products_split[1])
+        product_range = [str(i) for i in range(beginning, end + 1)]
+
+        valid_ids = validate_ids_part2(product_range)
+        if valid_ids:
+            answers.extend(valid_ids)
+    
 
     print(sum(answers))
--- 2025/day2/test_day2.py
+++ 2025/day2/test_day2.py
@@ -1,12 +1,44 @@
 import pytest
 
-from day2 import validate_ids
+from day2 import validate_ids_part1
 
-def test_first_range():
+def validate_ids_part2(id_range):
+    
+    minimum = 1
+    maximum = len(id_range[-1]) // 2
+    pattern_length_options = [i for i in range(minimum, maximum + 1)]
+
+    valid_ids = []
+
+    for len_option in pattern_length_options:    
+        for item in id_range:
+
+            pattern_length = len_option
+
+            pattern = item[0:pattern_length]
+            content = item[pattern_length:len(item)]
+
+            if content:
+                split_content = content.split(pattern)
+                test = [i for i in split_content if i != '']
+                if not test:
+                    if item not in valid_ids:
+                        valid_ids.append(item)
+
+    return valid_ids
+
+def test_part1_ranges():
 
     sut = [str(i) for i in range(1188511880, 1188511891)]
 
-    valid_ids = validate_ids(sut)
+    valid_ids = validate_ids_part1(sut)
 
     assert len(valid_ids) == 1
-            
+
+def test_part2_ranges():
+
+    sut = [str(i) for i in range(1, 21)]
+
+    valid_ids = validate_ids_part2(sut)
+
+    assert len(valid_ids) == 1
