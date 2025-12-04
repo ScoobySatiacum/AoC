de2ea103370f90891dcc9edd33da56e82d0bc961
Skyler Satiacum <skylersatiacum@gmail.com>
2025-12-04 10:41:01 -0800

    2025 day 4 done

--- 2025/day4/day4.py
+++ 2025/day4/day4.py
@@ -1,11 +1,9 @@
-if __name__ == '__main__':
+import copy
 
-    inputs = []
-    with open(r'C:\Users\skyle\Documents\dev\AoC\2025\day4\input.txt', 'r', encoding='utf-8') as fo:
-        inputs = [r.replace("\n", "") for r in fo.readlines()]
+def solve(inputs):
 
-    accessible_rolls = 0
-    
+    new_inputs = copy.deepcopy(inputs)
+
     directions = {
         'N': (0, -1),
         'NE': (1, -1),
@@ -16,6 +14,8 @@ if __name__ == '__main__':
         'W': (-1, 0),
         'NW': (-1, -1)
     }
+
+    accessible_rolls = 0
 
     for i in range(len(inputs)):
         for j in range(len(inputs[i])):
@@ -34,5 +34,30 @@ if __name__ == '__main__':
                                 pass
                 if roll_count < 4:
                     accessible_rolls += 1
+                    new_inputs[i][j] = '.'
+    
+    return accessible_rolls, new_inputs
 
-    print(accessible_rolls)
+def part2(accessible_rolls, total, inputs):
+    if accessible_rolls == 0:
+        return total
+    else:
+        accessible_rolls, inputs = solve(inputs)
+        if accessible_rolls == 0:
+            print(total)
+            return total
+        else:
+            total += accessible_rolls
+            part2(accessible_rolls, total, inputs)
+
+
+if __name__ == '__main__':
+
+    inputs = []
+    with open(r'C:\Users\skyle\Documents\dev\AoC\2025\day4\input.txt', 'r', encoding='utf-8') as fo:
+        inputs = [r.replace("\n", "") for r in fo.readlines()]
+    
+    inputs = [list(i) for i in inputs]
+    
+    #print(solve(inputs)) 
+    print(part2(-1, 0, inputs))
