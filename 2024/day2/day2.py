7d740a6f1a7bb9d64ea8a98b231a54c7e5dd199e
Skyler Satiacum <skylersatiacum@gmail.com>
2025-12-07 13:44:15 -0800

    Moving in the right direction

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -1,79 +1,81 @@
 def validate_report(report):
-    report_levels = report.split(' ')
-    report_levels = [int(i) for i in report_levels]
 
-    direction = 'increasing' if report_levels[0] < report_levels[1] else 'decreasing'
+    direction = 'increasing' if report_levels[0] < report_levels[-1] else 'decreasing'
     
     for i in range(len(report_levels) - 1):
         if report_levels[i] > report_levels[i + 1]:
             if direction == 'increasing':
-                return False
+                return False, i
             else:
                 if report_levels[i] - report_levels[i + 1] > 0:
                     if report_levels[i] - report_levels[i + 1] < 4:
                         pass
                     else:
-                        return False
+                        return False, i
                 else:
-                    return False
+                    return False, i
         else:
             if direction == 'decreasing':
-                return False
+                return False, i
             else:
                 if report_levels[i + 1] - report_levels[i] > 0:
                     if report_levels[i + 1] - report_levels[i] < 4:
                         pass
                     else:
-                        return False
+                        return False, i
                 else:
-                    return False
+                    return False, i
     
+    return True, -1
+
+def validate_direction(report):
+    direction = None
+    for i in range(len(report) - 1):
+        if report[i] > report[i + 1]:
+            if not direction:
+                direction = 'decreasing'
+            else:
+                if direction != 'decreasing':
+                    return False, i
+        else:
+            if not direction:
+                direction = 'increasing'
+            else:
+                if direction != 'increasing':
+                    return False, i
+    return True, -1
+
+def create_differences(report):
+    differences = []
+    for i in range(len(report) - 1):
+        differences.append(report[i + 1] - report[i])
+    return differences
+
+def difference_validation(differences):
+    test = [i for i in differences if i > 3 or i < -3]
+
+    return test
+
+def validate_range(differences, report, first_run = True):    
+    
+    # test for existence of 0, meaning sequential numbers
+    if 0 in differences:
+        if differences.count(0) > 1:
+            return False
+        
+    test = difference_validation(differences)
+    if len(test) > 1:
+        return False
+    if len(test) == 1:
+        report = [i for i in report if differences.index(test[0]) + 1 != i]
+        differences = create_differences(report)
+        test = difference_validation(differences)
+        if test:
+            return False
+        
     return True
+            
 
-def part2(report):
-    report_levels = report.split(' ')
-    report_levels = [int(i) for i in report_levels]
-
-    direction = 'increasing' if report_levels[0] < report_levels[1] else 'decreasing'
-    
-    bad_report = False
-    for i in range(len(report_levels) - 1):
-        if report_levels[i] > report_levels[i + 1]:
-            if direction == 'increasing':
-                return False
-            else:
-                if report_levels[i] - report_levels[i + 1] > 0:
-                    if report_levels[i] - report_levels[i + 1] < 4:
-                        pass
-                    else:
-                        if not bad_report:
-                            bad_report = True
-                        else:
-                            return False
-                else:
-                    if not bad_report:
-                        bad_report = True
-                    else:
-                        return False
-        else:
-            if direction == 'decreasing':
-                return False
-            else:
-                if report_levels[i + 1] - report_levels[i] > 0:
-                    if report_levels[i + 1] - report_levels[i] < 4:
-                        pass
-                    else:
-                        if not bad_report:
-                            bad_report = True
-                        else:
-                            return False
-                else:
-                    if not bad_report:
-                        bad_report = True
-                    else:
-                        return False
-    
-    return True    
 
 
 if __name__ == '__main__':
@@ -88,20 +90,27 @@ if __name__ == '__main__':
     unsafe_reports = []
 
     for report in inputs:
-        if validate_report(report):
+        report_levels = report.split(' ')
+        report_levels = [int(i) for i in report_levels]
+        result, bad_index = validate_report(report_levels)
+        if result:
             safe_reports.append(report)
         else:
             unsafe_reports.append(report)
 
     print(len(safe_reports))
 
+    direction_safe_reports = []
     safe_reports = []
+    unsafe_reports = []
 
     for report in inputs:
-        if part2(report):
-            safe_reports.append(report)
-        else:
-            unsafe_reports.append(report)
+        report_levels = report.split(' ')
+        report_levels = [int(i) for i in report_levels]
+
+        differences = create_differences(report_levels)
+        result = validate_range(differences, report_levels)
 
     print(len(safe_reports))
-    print(unsafe_reports)
+    for report in unsafe_reports:
+        print(report)
